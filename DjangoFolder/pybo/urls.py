67e80e90b55327f2dcac94118154fe3db6ca3c8c
sonsubin <ttokky0129@gmail.com>
2023-07-06 00:25:35 +0900

    question_create

--- DjangoFolder/pybo/urls.py
+++ DjangoFolder/pybo/urls.py
@@ -4,9 +4,10 @@ from . import views
 app_name='pybo'
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    path('question/create/', views.question_create, name='question_create'),
 ]
 # config/urls.py 에서 건네 받고 그 뒤에 ""이면 views.py의 index를 실행해라!
 # name 은 별칭!
