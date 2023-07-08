493510d760c9387c40f29c65d2b182f8f2e63a89
sonsubin <ttokky0129@gmail.com>
2023-07-08 17:03:22 +0900

    calculator 예제 수정

--- DjangoPractice/demos/views.py
+++ DjangoPractice/demos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.handlers.wsgi import WSGIRequest
 
 # Create your views here.
 
@@ -8,23 +8,22 @@ def calculator(request):
     #return HttpResponse("계산기 기능 구현 시작합니다^^")
     
     # 1. 데이터 확인
-    num1=int(request.GET.get('num1'))
-    num2=int(request.GET.get('num2'))
+    num1=request.GET.get('num1')
+    num2=request.GET.get('num2')
     operators = request.GET.get('operators')
 
     # 2. 계산
     if operators =='+':
-        result = num1+num2 
+        result = int(num1)+int(num2)
     elif operators =='-':
-        result = num1-num2
+        result = int(num1)-int(num2)
     elif operators =='*':
-        result = num1*num2
+        result = int(num1)*int(num2)
     elif operators =='/':
-        result = num1/num2
+        result = int(num1)/int(num2)
     else:
         result=0
 
     # 3. 응답
-
     return render(request, 'calculator.html',{'result':result})
     #result라는 key에 result 값을 넘겨줌
