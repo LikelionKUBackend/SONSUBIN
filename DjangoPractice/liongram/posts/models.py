4419bc570d52dc69dd0fe5776efc55770f4ca90a
sonsubin <ttokky0129@gmail.com>
2023-07-21 00:32:08 +0900

    model 작성

--- DjangoPractice/liongram/posts/models.py
+++ DjangoPractice/liongram/posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    image = models.ImageField(verbose_name='이미지')
+    content = models.TextField('내용')
+    created_at = models.DateTimeField('작성일')
+    view_count =  models.IntegerField('조회수')
