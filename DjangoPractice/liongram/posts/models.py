0e8d7741b8d850311d19faaae08e88904708bf67
sonsubin <ttokky0129@gmail.com>
2023-07-21 00:57:00 +0900

    shell을 통해 database 추가해보기

--- DjangoPractice/liongram/posts/models.py
+++ DjangoPractice/liongram/posts/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User=get_user_model()
 
 # Create your models here.
 class Post(models.Model):
-    image = models.ImageField(verbose_name='이미지')
-    content = models.TextField('내용')
-    created_at = models.DateTimeField('작성일')
-    view_count =  models.IntegerField('조회수')
+    image = models.ImageField(verbose_name='이미지', null=True,blank=True)
+    content = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(verbose_name='작성일',auto_now_add=True)
+    view_count =  models.IntegerField(verbose_name='조회수',default=0)
+    writer = models.ForeignKey(to=User, on_delete=models.CASCADE,null=True, blank=True)
+
+class Comment(models.Model):
+    content = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(verbose_name='작성일')
+    post = models.ForeignKey(to='POST',on_delete=models.CASCADE) #게시글이 지워지면 댓글도 전부 지워지는
+    writer = models.ForeignKey(to=User, on_delete=models.CASCADE)
