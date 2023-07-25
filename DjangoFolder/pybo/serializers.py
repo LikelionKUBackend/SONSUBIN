2f39f5503bab3304727684e1fe07c27d6a5f1dd2
sonsubin <ttokky0129@gmail.com>
2023-07-25 19:13:51 +0900

    serialize

---
+++ DjangoFolder/pybo/serializers.py
@@ -0,0 +1,7 @@
+#product/serializers.py
+from rest_framework import serializers
+from .models import Question
+class QuestionSerializer(serializers.ModelSerializer) : 
+    class Meta :
+        model=Question
+        fields='__all__'
